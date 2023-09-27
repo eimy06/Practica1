9d89b6581e5ba6662b6b89052c97360c42cac740
eimy06 <eimylaurafigueroa@gmail.com>
2023-09-26 22:21:23 -0400

    Actualizar sumar.py en la rama dev

--- sumar.py
+++ sumar.py
@@ -1,1 +1,8 @@
-"Este es el contenido de mi archivo." 
+def sumar(a, b):
+    return a + b
+
+numero1 = float(input("Ingresa el primer número: "))
+numero2 = float(input("Ingresa el segundo número: "))
+
+resultado = sumar(numero1, numero2)
+print("La suma es:", resultado)
